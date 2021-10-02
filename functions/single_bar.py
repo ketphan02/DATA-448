6212adcf473c80ff1194e6a248f00d249ba9a574
Restyled.io <commits@restyled.io>
2021-10-02 23:52:04 +0000

    Restyled by autopep8

--- functions/single_bar.py
+++ functions/single_bar.py
@@ -5,7 +5,8 @@ import numpy as np
 
 colors = sns.color_palette("Set2")
 
-def viz(df, class_avg, field, name, save_dir, max_num = 5):
+
+def viz(df, class_avg, field, name, save_dir, max_num=5):
     data = df[field]
     team_avg = sum(data) / len(data)
     avg = class_avg[field]
@@ -14,16 +15,16 @@ def viz(df, class_avg, field, name, save_dir, max_num = 5):
         'Score': [team_avg, avg],
     })
 
-    fig, ax = plt.subplots(figsize=(6.5, 6)) 
+    fig, ax = plt.subplots(figsize=(6.5, 6))
 
     ax = (
         sns
         .barplot(data=data, x='Name', y='Score')
         .set(xlabel='', ylabel=field, yticks=np.arange(0, max_num + 1).tolist())
     )
-    
+
     plt.title(name, size=20)
     plt.ylabel(field, size=16)
-    
+
     plt.show()
     fig.savefig(save_dir, dpi=300)
