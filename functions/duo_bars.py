529fbfde8c184767373cd7fd4ca7368ab87a10a7
Kiet Phan <tuankietvn@outlook.com>
2021-10-03 15:15:14 -0700

    Fix indentation bug

--- functions/duo_bars.py
+++ functions/duo_bars.py
@@ -5,65 +5,46 @@ import seaborn as sns
 colors = sns.color_palette("Set2")
 
 def viz(df, class_avg, name, save_dir, max_len = 15):
-	"""
+	'''
 	Visualize class average versus team average by duobars.
 
 	Parameters
-    ----------
-    df : pandas.DataFrame
-        DataFrame containing the data.
-    class_avg : pandas.DataFrame
-        A dataframe that contain all average of all fields.
-    name : str
-        Title of the plot.
-    save_dir : str
-        Directory to save the visualizaton.
-    max_len : int (default=15)
-        Maximum length of the y axis.
-    """
-    # Get the data
-    data = df.groupby("team").mean()
-    data = data.sort_values(by = "avg", ascending = False)
-    data = data.reset_index()
-    data = data.iloc[:max_len]
+	----------
+	df : pandas.DataFrame
+		DataFrame containing the data.
+	class_avg : pandas.DataFrame
+		A dataframe that contain all average of all fields.
+	name : str
+		Title of the plot.
+	save_dir : str
+		Directory to save the visualizaton.
+	max_len : int (default=15)
+		Maximum length of the y axis.
+	'''
+	
+	data = df.drop(columns='Name')
+	labels = data.columns
+	data = sum(data.values)
+	class_avg = class_avg.drop(columns='Name').values[0].tolist()
 
-    # Plot
-    plt.figure(figsize = (12, 8))
-    plt.title(f"{name}")
-    plt.barh(data["team"], data["avg"], color = colors[0], label = "Class Average")
-    plt.barh(data["team"], data["avg"] - class_avg, color = colors[1], label = "Team Average")
-    plt.barh(data["team"], data["avg"] + class_avg, color = colors[2], label = "Class Average")
-    plt.legend()
-    plt.xlabel("Average")
-    plt.ylabel("Team")
-    plt.savefig(f"{save_dir}/{name}.png")
-    plt.close()
-	"""
+	space = 1
+	each_col_width = 3
+	n = len(data)
+	# graph_length = each_col_width * 2 * n + space * (n  + 1)
 
+	bar_loc = [((i + 1) * space) + (i * 2 * each_col_width) + each_col_width for i in range(n)]
+	x_left = [x - each_col_width /2 for x in bar_loc]
+	x_right = [x + each_col_width /2 for x in bar_loc]
 
-    data = df.drop(columns='Name')
-    labels = data.columns
-    data = sum(data.values)
-    class_avg = class_avg.drop(columns='Name').values[0].tolist()
+	fig, ax = plt.subplots(figsize=(9, 6))
+	ax.bar(height = data, x=x_left, color = colors[0], align='center', width=each_col_width)
+	ax.bar(height = class_avg, x=x_right, color = colors[1], align='center', width=each_col_width)
 
-    space = 1
-    each_col_width = 3
-    n = len(data)
-    # graph_length = each_col_width * 2 * n + space * (n  + 1)
-
-    bar_loc = [((i + 1) * space) + (i * 2 * each_col_width) + each_col_width for i in range(n)]
-    x_left = [x - each_col_width /2 for x in bar_loc]
-    x_right = [x + each_col_width /2 for x in bar_loc]
-
-    fig, ax = plt.subplots(figsize=(9, 6))
-    ax.bar(height = data, x=x_left, color = colors[0], align='center', width=each_col_width)
-    ax.bar(height = class_avg, x=x_right, color = colors[1], align='center', width=each_col_width)
-
-    plt.xticks(bar_loc)
-    ax.set_xticklabels(labels, fontsize=14)
-    plt.yticks(np.arange(0, max_len + 1).tolist())
-    plt.title(name, fontsize=20)
-    plt.legend(['Team Average', 'Class average'], fontsize=12)
-    
-    plt.show()
-    fig.savefig(save_dir, dpi=300)
+	plt.xticks(bar_loc)
+	ax.set_xticklabels(labels, fontsize=14)
+	plt.yticks(np.arange(0, max_len + 1).tolist())
+	plt.title(name, fontsize=20)
+	plt.legend(['Team Average', 'Class average'], fontsize=12)
+	
+	plt.show()
+	fig.savefig(save_dir, dpi=300)
