39645c1e5b992f6dc57adcce99f56752ed5864b5
Restyled.io <commits@restyled.io>
2021-11-03 08:38:52 +0000

    Restyled by black

--- functions/section2/grid_view.py
+++ functions/section2/grid_view.py
@@ -19,9 +19,7 @@ def viz(csv_file, save_dir):
     fig = plt.figure(figsize=(20, 7))
     for i in range(len(df.columns.values)):
         ax = fig.add_subplot(2, len(df.columns.values) // 2, i + 1)
-        plt.bar(height=[0, 4, 0],
-                x=["foo", df.columns.values[i], "bar"],
-                color="white")
+        plt.bar(height=[0, 4, 0], x=["foo", df.columns.values[i], "bar"], color="white")
         plt.bar(
             height=[0, df.loc[4].values[i], 0],
             x=["foo", df.columns.values[i], "bar"],
@@ -54,33 +52,33 @@ def viz(csv_file, save_dir):
             if data[j] == 1:
                 avail.append(labels[j])
 
-        info.append([data[0], 'Choose:\n' + '\n'.join(avail)])
+        info.append([data[0], "Choose:\n" + "\n".join(avail)])
 
     fig, ax = plt.subplots(figsize=(10, 10))
-    table = ax.table(info, loc='center')
+    table = ax.table(info, loc="center")
     table.set_fontsize(14)
     table.scale(1, 12)
-    ax.axis('off')
+    ax.axis("off")
 
-    fig.savefig('../../visualizations/section 2/temp/dataframe.png',
-                dpi=300, bbox_inches='tight', pad_inches=0)
+    fig.savefig(
+        "../../visualizations/section 2/temp/dataframe.png",
+        dpi=300,
+        bbox_inches="tight",
+        pad_inches=0,
+    )
 
     # Merge
     image1 = cv2.imread("../../visualizations/section 2/temp/gridview.png")
     image2 = cv2.imread("../../visualizations/section 2/temp/dataframe.png")
     image2 = cv2.copyMakeBorder(
-        image2, 0, 50, 200, 200, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        image2, 0, 50, 200, 200, cv2.BORDER_CONSTANT, value=[255, 255, 255]
+    )
     ratio = image1.shape[1] / image2.shape[1]
     size = (int(image1.shape[1] / ratio), int(image1.shape[0] / ratio))
     image1 = cv2.resize(image1, size, cv2.INTER_NEAREST)
 
     image = cv2.vconcat([image1, image2])
-    image = cv2.copyMakeBorder(image,
-                               0,
-                               20,
-                               0,
-                               0,
-                               cv2.BORDER_CONSTANT,
-                               None,
-                               value=[255, 255, 255])
+    image = cv2.copyMakeBorder(
+        image, 0, 20, 0, 0, cv2.BORDER_CONSTANT, None, value=[255, 255, 255]
+    )
     cv2.imwrite(save_dir, image)
--- functions/section2/heat_map.py
+++ functions/section2/heat_map.py
@@ -23,9 +23,7 @@ def viz(csv_file, save_dir):
     ax, fig = plt.subplots(figsize=(8, 3))
     ax = sns.heatmap(df, cmap=make_colormap(), yticklabels=False)
     ax.collections[0].colorbar.set_ticks([0.0, 1.0, 2.0, 3.0, 4.0])
-    ax.set_xticklabels(ax.get_xticklabels(),
-                       rotation=50,
-                       horizontalalignment="right")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=50, horizontalalignment="right")
     plt.tight_layout()
     plt.savefig("../../visualizations/section 2/temp/heatmap.png", dpi=300)
 
@@ -40,32 +38,32 @@ def viz(csv_file, save_dir):
             if data[j] == 1:
                 avail.append(labels[j])
 
-        info.append([data[0], 'Choose:\n' + '\n'.join(avail)])
+        info.append([data[0], "Choose:\n" + "\n".join(avail)])
 
     fig, ax = plt.subplots(figsize=(10, 10))
-    table = ax.table(info, loc='center')
+    table = ax.table(info, loc="center")
     table.set_fontsize(14)
     table.scale(1, 12)
-    ax.axis('off')
+    ax.axis("off")
 
-    fig.savefig('../../visualizations/section 2/temp/dataframe_1.png',
-                dpi=300, bbox_inches='tight', pad_inches=0)
+    fig.savefig(
+        "../../visualizations/section 2/temp/dataframe_1.png",
+        dpi=300,
+        bbox_inches="tight",
+        pad_inches=0,
+    )
 
     # Merge
     image1 = cv2.imread("../../visualizations/section 2/temp/heatmap.png")
     image2 = cv2.imread("../../visualizations/section 2/temp/dataframe_1.png")
     image2 = cv2.copyMakeBorder(
-        image2, 0, 50, 200, 200, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        image2, 0, 50, 200, 200, cv2.BORDER_CONSTANT, value=[255, 255, 255]
+    )
     ratio = image1.shape[1] / image2.shape[1]
     size = (int(image1.shape[1] / ratio), int(image1.shape[0] / ratio))
     image1 = cv2.resize(image1, size, cv2.INTER_NEAREST)
     image = cv2.vconcat([image1, image2])
-    image = cv2.copyMakeBorder(image,
-                               0,
-                               20,
-                               0,
-                               0,
-                               cv2.BORDER_CONSTANT,
-                               None,
-                               value=[255, 255, 255])
+    image = cv2.copyMakeBorder(
+        image, 0, 20, 0, 0, cv2.BORDER_CONSTANT, None, value=[255, 255, 255]
+    )
     cv2.imwrite(save_dir, image)
