a636c271ddae6e334026e7fe8c6bf11df2bc5dbc
Kiet Phan <tuankietvn@outlook.com>
2021-11-03 01:37:46 -0700

    Add new tally

--- functions/section2/grid_view.py
+++ functions/section2/grid_view.py
@@ -2,6 +2,8 @@ import cv2
 import dataframe_image as dfi
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+from IPython.display import display
 
 
 def viz(csv_file, save_dir):
@@ -33,7 +35,7 @@ def viz(csv_file, save_dir):
         ax.spines["bottom"].set_color("black")
         if i < len(df.columns.values) // 2:
             ax.xaxis.tick_top()
-        plt.xticks([df.columns.values[i]], fontsize=15)
+        plt.xticks([df.columns.values[i]], fontsize=20)
         plt.ylim((0, 4))
         plt.yticks([])
 
@@ -42,14 +44,36 @@ def viz(csv_file, save_dir):
     fig.savefig("../../visualizations/section 2/temp/gridview.png", dpi=300)
 
     # Produce tally
-    dfi.export(df_display, "../../visualizations/section 2/temp/dataframe.png")
+    info = []
+    labels = df_display.columns.tolist()
+    for i in range(df_display.shape[0]):
+        avail = []
+        data = df_display.loc[i]
+
+        for j in range(1, len(data)):
+            if data[j] == 1:
+                avail.append(labels[j])
+
+        info.append([data[0], 'Choose:\n' + '\n'.join(avail)])
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    table = ax.table(info, loc='center')
+    table.set_fontsize(14)
+    table.scale(1, 12)
+    ax.axis('off')
+
+    fig.savefig('../../visualizations/section 2/temp/dataframe.png', dpi=300, bbox_inches='tight',pad_inches = 0)
 
     # Merge
     image1 = cv2.imread("../../visualizations/section 2/temp/gridview.png")
     image2 = cv2.imread("../../visualizations/section 2/temp/dataframe.png")
+    image2 = cv2.copyMakeBorder(
+        image2, 0, 50, 200, 200, cv2.BORDER_CONSTANT, value=[255, 255, 255])
     ratio = image1.shape[1] / image2.shape[1]
     size = (int(image1.shape[1] / ratio), int(image1.shape[0] / ratio))
     image1 = cv2.resize(image1, size, cv2.INTER_NEAREST)
+
+
     image = cv2.vconcat([image1, image2])
     image = cv2.copyMakeBorder(image,
                                0,
