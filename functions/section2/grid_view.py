22309d652738efaa2881982cbb49a299c2d412fb
Kiet Phan <tuankietvn@outlook.com>
2021-11-09 15:21:30 -0800

    Fix issues

--- functions/section2/grid_view.py
+++ functions/section2/grid_view.py
@@ -1,14 +1,14 @@
 import cv2
-import dataframe_image as dfi
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
-from IPython.display import display
+
+from functions.section2 import tally
 
 
-def viz(csv_file, save_dir):
+def viz(csv_file, title, save_dir, need_tally = False):
     df = pd.read_csv(csv_file, sep=";")
     df_display = df
+    num_ppl = df.shape[0]
     df_names = df["Name"].tolist()
     df_names.append("Total")
     df_rows = df.shape[0]
@@ -19,15 +19,15 @@ def viz(csv_file, save_dir):
     fig = plt.figure(figsize=(20, 7))
     for i in range(len(df.columns.values)):
         ax = fig.add_subplot(2, len(df.columns.values) // 2, i + 1)
-        plt.bar(height=[0, 4, 0],
+        plt.bar(height=[0, num_ppl, 0],
                 x=["foo", df.columns.values[i], "bar"],
                 color="white")
         plt.bar(
-            height=[0, df.loc[4].values[i], 0],
+            height=[0, df.loc[num_ppl].values[i], 0],
             x=["foo", df.columns.values[i], "bar"],
             color="#FF6B6B",
         )
-        plt.yticks([0, 4])
+        plt.yticks([0, num_ppl])
         plt.xlim([df.columns.values[i], df.columns.values[i]])
         ax.spines["left"].set_color("black")
         ax.spines["top"].set_color("black")
@@ -36,60 +36,33 @@ def viz(csv_file, save_dir):
         if i < len(df.columns.values) // 2:
             ax.xaxis.tick_top()
         plt.xticks([df.columns.values[i]], fontsize=20)
-        plt.ylim((0, 4))
+        plt.ylim((0, num_ppl))
         plt.yticks([])
 
+    fig.suptitle(title, fontsize = 50)
+    plt.tight_layout()
     plt.subplots_adjust(wspace=0, hspace=0)
-    #     plt.show()
-    fig.savefig("../../visualizations/section 2/temp/gridview.png", dpi=300)
 
-    # Produce tally
-    info = []
-    labels = df_display.columns.tolist()
-    for i in range(df_display.shape[0]):
-        avail = []
-        data = df_display.loc[i]
+    if need_tally:
+        fig.savefig("../../visualizations/section 2/temp/gridview.png", dpi=300)
 
-        for j in range(1, len(data)):
-            if data[j] == 1:
-                avail.append(labels[j])
+        # Produce tally
+        tally.viz(df_display, "../../visualizations/section 2/temp/dataframe.png")
 
-        info.append([data[0], "Choose:\n" + "\n".join(avail)])
-
-    fig, ax = plt.subplots(figsize=(10, 10))
-    table = ax.table(info, loc="center")
-    table.set_fontsize(14)
-    table.scale(1, 12)
-    ax.axis("off")
-
-    fig.savefig(
-        "../../visualizations/section 2/temp/dataframe.png",
-        dpi=300,
-        bbox_inches="tight",
-        pad_inches=0,
-    )
-
-    # Merge
-    image1 = cv2.imread("../../visualizations/section 2/temp/gridview.png")
-    image2 = cv2.imread("../../visualizations/section 2/temp/dataframe.png")
-    image2 = cv2.copyMakeBorder(image2,
-                                0,
-                                50,
-                                200,
-                                200,
-                                cv2.BORDER_CONSTANT,
-                                value=[255, 255, 255])
-    ratio = image1.shape[1] / image2.shape[1]
-    size = (int(image1.shape[1] / ratio), int(image1.shape[0] / ratio))
-    image1 = cv2.resize(image1, size, cv2.INTER_NEAREST)
-
-    image = cv2.vconcat([image1, image2])
-    image = cv2.copyMakeBorder(image,
-                               0,
-                               20,
-                               0,
-                               0,
-                               cv2.BORDER_CONSTANT,
-                               None,
-                               value=[255, 255, 255])
-    cv2.imwrite(save_dir, image)
+        # Merge
+        image1 = cv2.imread("../../visualizations/section 2/temp/gridview.png")
+        image2 = cv2.imread("../../visualizations/section 2/temp/dataframe.png")
+        image2 = cv2.copyMakeBorder(image2,
+                                    0,
+                                    0,
+                                    100,
+                                    100,
+                                    cv2.BORDER_CONSTANT,
+                                    value=[255, 255, 255])
+        ratio = image1.shape[1] / image2.shape[1]
+        size = (int(image1.shape[1] / ratio), int(image1.shape[0] / ratio))
+        image1 = cv2.resize(image1, size, cv2.INTER_NEAREST)
+        image = cv2.vconcat([image1, image2])
+        cv2.imwrite(save_dir, image)
+    else:
+        fig.savefig(save_dir, dpi=300)
--- functions/section2/heat_map.py
+++ functions/section2/heat_map.py
@@ -1,80 +1,56 @@
 import cv2
-import dataframe_image as dfi
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
+from functions.section2 import tally
 
 def make_colormap():
     #     colors = ["#76EAD7", "#4CD3C2","#2FC4B2", "#12947F"] # blue
-    colors = ["#FCD2D1", "#FE8F8F", "#FF5C58", "#F54748"]  # red
+    colors = ["#FFFFFF", "#FFB344", "#F54748"]  # red
     return mcolors.LinearSegmentedColormap.from_list("", colors)
 
 
-def viz(csv_file, save_dir):
+def viz(csv_file, title, save_dir, need_tally = False):
     df = pd.read_csv(csv_file, sep=";")
     df_display = df
+    num_user = df.shape[0]
     df = df.drop(labels="Name", axis=1)
     df.loc["No. of members"] = df.sum(axis=0)
-    df = df.drop(labels=[0, 1, 2, 3], axis=0)
+    df = df.drop(labels=np.arange(0, num_user, 1), axis=0)
 
     # Produce gridview
-    ax, fig = plt.subplots(figsize=(8, 3))
-    ax = sns.heatmap(df, cmap=make_colormap(), yticklabels=False)
-    ax.collections[0].colorbar.set_ticks([0.0, 1.0, 2.0, 3.0, 4.0])
+    ax, fig = plt.subplots(figsize=(9, 4.5))
+    ax = sns.heatmap(df, cmap=make_colormap(), yticklabels=False, vmin=0, vmax=num_user)
+    ax.collections[0].colorbar.set_ticks(np.arange(0, num_user + 1, 1))
     ax.set_xticklabels(ax.get_xticklabels(),
                        rotation=50,
                        horizontalalignment="right")
+    plt.title(title, fontsize=25, y=1.1)
     plt.tight_layout()
-    plt.savefig("../../visualizations/section 2/temp/heatmap.png", dpi=300)
 
-    # Produce tally
-    info = []
-    labels = df_display.columns.tolist()
-    for i in range(df_display.shape[0]):
-        avail = []
-        data = df_display.loc[i]
+    if need_tally:
+        plt.savefig("../../visualizations/section 2/temp/heatmap.png", dpi=300)
 
-        for j in range(1, len(data)):
-            if data[j] == 1:
-                avail.append(labels[j])
+        # Produce tally
+        tally.viz(df_display, '../../visualizations/section 2/temp/dataframe_1.png')
 
-        info.append([data[0], "Choose:\n" + "\n".join(avail)])
-
-    fig, ax = plt.subplots(figsize=(10, 10))
-    table = ax.table(info, loc="center")
-    table.set_fontsize(14)
-    table.scale(1, 12)
-    ax.axis("off")
-
-    fig.savefig(
-        "../../visualizations/section 2/temp/dataframe_1.png",
-        dpi=300,
-        bbox_inches="tight",
-        pad_inches=0,
-    )
-
-    # Merge
-    image1 = cv2.imread("../../visualizations/section 2/temp/heatmap.png")
-    image2 = cv2.imread("../../visualizations/section 2/temp/dataframe_1.png")
-    image2 = cv2.copyMakeBorder(image2,
-                                0,
-                                50,
-                                200,
-                                200,
-                                cv2.BORDER_CONSTANT,
-                                value=[255, 255, 255])
-    ratio = image1.shape[1] / image2.shape[1]
-    size = (int(image1.shape[1] / ratio), int(image1.shape[0] / ratio))
-    image1 = cv2.resize(image1, size, cv2.INTER_NEAREST)
-    image = cv2.vconcat([image1, image2])
-    image = cv2.copyMakeBorder(image,
-                               0,
-                               20,
-                               0,
-                               0,
-                               cv2.BORDER_CONSTANT,
-                               None,
-                               value=[255, 255, 255])
-    cv2.imwrite(save_dir, image)
+        # Merge
+        image1 = cv2.imread("../../visualizations/section 2/temp/heatmap.png")
+        image2 = cv2.imread("../../visualizations/section 2/temp/dataframe_1.png")
+        image2 = cv2.copyMakeBorder(image2,
+                                    0,
+                                    0,
+                                    100,
+                                    100,
+                                    cv2.BORDER_CONSTANT,
+                                    value=[255, 255, 255])
+        ratio = image1.shape[1] / image2.shape[1]
+        size = (int(image1.shape[1] / ratio), int(image1.shape[0] / ratio))
+        image1 = cv2.resize(image1, size, cv2.INTER_NEAREST)
+        image = cv2.vconcat([image1, image2])
+        cv2.imwrite(save_dir, image)
+    else:
+        plt.savefig(save_dir, dpi=300)
